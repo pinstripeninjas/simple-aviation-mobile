6523fcb4d0c42fffaed33f294c4069206b31eb53
pinstripeninjas <pinstripeninjas@gmail.com>
2020-05-06 16:47:11 -0700

    initial commit

---
+++ js/aviationMatrixMobile.js
@@ -0,0 +1,61 @@
+// API url locations
+const urlForecast = "./json/forecast.json";
+const urlCriteria = "./json/criteria.json";
+
+const matrix = document.querySelector("#matrix");
+
+const getDataForMatrix = () => {
+	axios
+		.all([
+			axios({
+				method: "get",
+				url: urlForecast,
+				onDownloadProgress: function () {
+					//reqFcst.innerHTML = "<span class='spinner-border'></span> Loading...";
+				},
+			}),
+			axios.get(urlCriteria),
+		])
+		.then(
+			axios.spread(({ data: forecast }, { data: criteria }) => {
+				buildMatrix(forecast, criteria);
+			})
+		);
+};
+
+const buildMatrix = (forecast, criteria) => {
+	const datesArray = buildDatesArray(forecast.validPeriod);
+	for (let i = 0; i < datesArray.length; i++) {
+		const dateDiv = document.createElement("div");
+		dateDiv.classList.add("date-header");
+		dateDiv.innerText = datesArray[i];
+		matrix.append(dateDiv);
+		fillDataForDay(forecast, criteria, i);
+	}
+};
+
+// loops through and constructs an array of the "Day - Date"
+const buildDatesArray = (validPeriod) => {
+	const datesArray = [];
+	for (let i = 0; i < validPeriod.day.length; i++) {
+		datesArray.push(validPeriod.day[i] + " - " + validPeriod.date[i]);
+	}
+	return datesArray;
+};
+
+// populates data for the matrix for one day
+const fillDataForDay = ({ forecast }, criteria, i) => {
+	for (let j = 0; j < forecast.length; j++) {
+		const matrixLineDiv = document.createElement("div");
+		matrixLineDiv.classList.add("matrix-line");
+		const fieldNameDiv = document.createElement("div");
+		const fieldValueDiv = document.createElement("div");
+		fieldNameDiv.innerText = forecast[j].fullName;
+		fieldValueDiv.innerText = forecast[j].values[i];
+		matrixLineDiv.append(fieldNameDiv);
+		matrixLineDiv.append(fieldValueDiv);
+		matrix.append(matrixLineDiv);
+	}
+};
+
+getDataForMatrix();
