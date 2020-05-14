beb58b5917fd7a30794f1a83d4092f53f6754c37
pinstripeninjas <pinstripeninjas@gmail.com>
2020-05-14 15:00:16 -0700

    added criteria colors and criteria popup

--- static/js/aviationMatrixMobile.js
+++ static/js/aviationMatrixMobile.js
@@ -3,6 +3,12 @@ const urlForecast = "./static/json/forecast.json";
 const urlCriteria = "./static/json/criteria.json";
 
 const matrix = document.querySelector("#matrix");
+const criteriaPopup = document.querySelector("#criteria-popup");
+const criteriaPopupName = document.querySelector(".criteria-name");
+const criteriaPopupTable = document.querySelector(".criteria-table");
+
+// variable to handle setTimeout/clearTimeout
+let timeout;
 
 const getDataForMatrix = () => {
 	axios
@@ -54,7 +60,63 @@ const fillDataForDay = ({ forecast }, criteria, i) => {
 		fieldValueDiv.innerText = forecast[j].values[i];
 		matrixLineDiv.append(fieldNameDiv);
 		matrixLineDiv.append(fieldValueDiv);
+		// apply color class
+		matrixLineDiv.classList.add(applyColorClass(forecast[j].values[i], criteria.body[j]));
+		// apply touchstart listener
+		matrixLineDiv.addEventListener("touchstart", (event) => {
+			populatePopup(event, criteria.body[j], forecast[j].fullName);
+			console.log(event);
+			// display popup after setTimout
+			timeout = setTimeout(() => showPopup(true), 800);
+		});
+		// apply touchend listener
+		matrixLineDiv.addEventListener("touchend", () => {
+			clearTimeout(timeout);
+			showPopup(false);
+		});
+
 		matrix.append(matrixLineDiv);
+	}
+};
+
+const applyColorClass = (value, criteria) => {
+	// check if variable is sfc wind to remove directional component
+	if (criteria.variable === "maxSfcWind") {
+		value = Number(value.slice(2));
+	}
+	// return color for associated class
+	if (value > criteria.yellow) {
+		return "red";
+	} else if (value > criteria.green) {
+		return "yellow";
+	} else {
+		return "green";
+	}
+};
+
+// creates the criteria popup information
+const populatePopup = (event, criteria, field) => {
+	// get position of click
+	const position = event.touches[0].clientY - 95;
+	// build criteria popup
+	criteriaPopupName.innerText = field;
+	criteriaPopupTable.innerHTML = "";
+	for (let i = 0; i < criteria.textArray.length; i++) {
+		const newDiv = document.createElement("div");
+		newDiv.innerText = criteria.textArray[i];
+		newDiv.classList.add(criteria.colorsClass[i]);
+		criteriaPopupTable.append(newDiv);
+	}
+	// reveal popup at proper position
+	criteriaPopup.style.top = `${position}px`;
+};
+
+// displays/hides popup
+const showPopup = (value) => {
+	if (value) {
+		criteriaPopup.classList.remove("hidden");
+	} else {
+		criteriaPopup.classList.add("hidden");
 	}
 };
 
